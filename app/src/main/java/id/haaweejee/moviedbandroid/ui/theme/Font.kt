50964f4ff5911081d333fa137d9792c5629734fc
Hendrawan Wibowo Jatmiko <52823787+haaweejee@users.noreply.github.com>
2023-07-27 21:56:41 +0700

    + add font family

---
+++ app/src/main/java/id/haaweejee/moviedbandroid/ui/theme/Font.kt
@@ -0,0 +1,18 @@
+package id.haaweejee.moviedbandroid.ui.theme
+
+import androidx.compose.ui.text.font.Font
+import androidx.compose.ui.text.font.FontFamily
+import androidx.compose.ui.text.font.FontStyle
+import androidx.compose.ui.text.font.FontWeight
+import id.haaweejee.moviedbandroid.R
+
+val latoFontFamily = FontFamily(
+    Font(R.font.lato_light, FontWeight.Light),
+    Font(R.font.lato_light_italic, FontWeight.Light, FontStyle.Italic),
+    Font(R.font.lato_thin, FontWeight.Thin),
+    Font(R.font.lato_thin_italic, FontWeight.Thin, FontStyle.Italic),
+    Font(R.font.lato_regular, FontWeight.Normal),
+    Font(R.font.lato_italic, FontWeight.Normal, FontStyle.Italic),
+    Font(R.font.lato_bold, FontWeight.Bold),
+    Font(R.font.lato_bold, FontWeight.Bold, FontStyle.Italic),
+)
