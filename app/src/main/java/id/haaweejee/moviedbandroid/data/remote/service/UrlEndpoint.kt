64d0fd78be8ec351ae6df6226b681b6e534bac79
Hendrawan Wibowo Jatmiko <52823787+haaweejee@users.noreply.github.com>
2023-07-30 20:13:10 +0700

    + add constant for baseurl and endpoint

---
+++ app/src/main/java/id/haaweejee/moviedbandroid/data/remote/service/UrlEndpoint.kt
@@ -0,0 +1,8 @@
+package id.haaweejee.moviedbandroid.data.remote.service
+
+val baseUrl = "https://api.themoviedb.org/3/"
+val moviesGenres = "genre/movie/list"
+val moviesDiscover = "discover/movie"
+val movieDetail = "movie/"
+val movieReviews = "/reviews"
+val movieVideos = "/videos"
