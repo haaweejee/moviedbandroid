c08eeb2a874250b0e75ee3cca1458b20139e8f16
Hendrawan Wibowo Jatmiko <52823787+haaweejee@users.noreply.github.com>
2023-08-21 15:02:42 +0700

    - delete entities

--- app/src/main/java/id/haaweejee/moviedbandroid/domain/entities/MovieDiscoverEntities.kt
+++
@@ -1,9 +0,0 @@
-package id.haaweejee.moviedbandroid.domain.entities
-
-data class MovieDiscoverEntities(
-    val idMovie: Int,
-    val titleMovie: String,
-    val posterMovie: String,
-    val releaseDateMovie: String,
-    val ratingMovie: Double,
-)
