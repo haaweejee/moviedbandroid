91f902359a3025a468d0ef3817bc7a29c4eb1cf3
Hendrawan Wibowo Jatmiko <52823787+haaweejee@users.noreply.github.com>
2023-08-21 15:01:18 +0700

    ! update new repository

--- app/src/main/java/id/haaweejee/moviedbandroid/data/repository/MovieDbRepositoryImpl.kt
+++ app/src/main/java/id/haaweejee/moviedbandroid/data/repository/MovieDbRepositoryImpl.kt
@@ -2,23 +2,34 @@ package id.haaweejee.moviedbandroid.data.repository
 
 import androidx.paging.Pager
 import androidx.paging.PagingConfig
+import androidx.paging.PagingData
 import id.haaweejee.moviedbandroid.core.base.BaseRepository
+import id.haaweejee.moviedbandroid.data.local.dao.MovieDao
+import id.haaweejee.moviedbandroid.data.local.dao.RatedMovieDao
+import id.haaweejee.moviedbandroid.data.local.entity.LocalMovieEntity
+import id.haaweejee.moviedbandroid.data.local.entity.RatedMovieEntity
+import id.haaweejee.moviedbandroid.data.remote.dto.response.account.AccountResponse
+import id.haaweejee.moviedbandroid.data.remote.dto.response.accountstate.AccountStateResponse
 import id.haaweejee.moviedbandroid.data.remote.dto.response.detail.DetailMovieResponse
 import id.haaweejee.moviedbandroid.data.remote.dto.response.genre.GenresResponse
+import id.haaweejee.moviedbandroid.data.remote.dto.response.movielist.MovieResponse
+import id.haaweejee.moviedbandroid.data.remote.dto.response.movielist.MoviesResponse
+import id.haaweejee.moviedbandroid.data.remote.dto.response.rating.PostRatingResponse
 import id.haaweejee.moviedbandroid.data.remote.dto.response.review.ReviewsResponse
 import id.haaweejee.moviedbandroid.data.remote.dto.response.video.VideoResponse
 import id.haaweejee.moviedbandroid.data.remote.paging.MoviePagingSource
+import id.haaweejee.moviedbandroid.data.remote.paging.RatedMoviePagingSource
 import id.haaweejee.moviedbandroid.data.remote.paging.ReviewPagingSource
 import id.haaweejee.moviedbandroid.data.remote.service.MovieDbNetwork
 import id.haaweejee.moviedbandroid.domain.repository.MovieDbRepository
 import kotlinx.coroutines.flow.Flow
 import kotlinx.coroutines.flow.flow
 import javax.inject.Inject
-import javax.inject.Singleton
 
-@Singleton
 class MovieDbRepositoryImpl @Inject constructor(
     private val network: MovieDbNetwork,
+    private val movieDao: MovieDao,
+    private val ratedMovieDao: RatedMovieDao,
 ) : BaseRepository(), MovieDbRepository {
 
     override fun getGenres(): Flow<GenresResponse> = flow {
@@ -71,4 +82,70 @@ class MovieDbRepositoryImpl @Inject constructor(
             }
         }
     }
+
+    override suspend fun insertMovieToBookmark(movie: LocalMovieEntity) =
+        movieDao.insertMovie(movie)
+
+    override suspend fun deleteMovieBookmarked(movieId: Int) =
+        movieDao.deleteMovieById(movieId)
+
+    override fun getMovieBookmarkById(movieId: Int): Flow<LocalMovieEntity?> =
+        movieDao.getMovieById(movieId)
+
+    override fun getMoviesBookmarked(): Flow<List<LocalMovieEntity>> =
+        movieDao.getAllMovies()
+    override fun getAccountDetail(): Flow<AccountResponse> =
+        flow {
+            safeNetworkCall {
+                emit(network.getAccountDetail())
+            }
+        }
+
+    override fun getMovieRecommendation(movieId: String): Flow<MoviesResponse> =
+        flow {
+            safeNetworkCall {
+                emit(network.getMovieRecommendation(movieId))
+            }
+        }
+
+    override fun postMovieRating(movieId: String, rating: Double): Flow<PostRatingResponse> =
+        flow {
+            safeNetworkCall {
+                emit(network.postMovieRating(movieId, rating))
+            }
+        }
+
+    override fun deleteMovieRating(movieId: String): Flow<PostRatingResponse> =
+        flow {
+            safeNetworkCall {
+                emit(network.deleteMovieRating(movieId))
+            }
+        }
+
+    override suspend fun insertRatedMovie(movie: RatedMovieEntity) =
+        ratedMovieDao.insertRatedMovie(movie)
+
+    override suspend fun deleteRatedMovieById(movieId: Int) =
+        ratedMovieDao.deleteRatedMovieById(movieId)
+
+    override fun getRatedMovieById(movieId: Int): Flow<RatedMovieEntity?> =
+        ratedMovieDao.getRatedMovieById(movieId)
+
+    override fun getAccountState(movieId: String): Flow<AccountStateResponse> =
+        flow {
+            safeNetworkCall {
+                emit(network.getAccountState(movieId))
+            }
+        }
+
+    override fun getMoviesRated(): Flow<PagingData<MovieResponse>> =
+        Pager(
+            config = PagingConfig(
+                pageSize = 20,
+                enablePlaceholders = false,
+            ),
+            pagingSourceFactory = {
+                RatedMoviePagingSource(network)
+            },
+        ).flow
 }
--- app/src/main/java/id/haaweejee/moviedbandroid/domain/repository/MovieDbRepository.kt
+++ app/src/main/java/id/haaweejee/moviedbandroid/domain/repository/MovieDbRepository.kt
@@ -1,9 +1,15 @@
 package id.haaweejee.moviedbandroid.domain.repository
 
 import androidx.paging.PagingData
-import id.haaweejee.moviedbandroid.data.remote.dto.response.genre.GenresResponse
+import id.haaweejee.moviedbandroid.data.local.entity.LocalMovieEntity
+import id.haaweejee.moviedbandroid.data.local.entity.RatedMovieEntity
+import id.haaweejee.moviedbandroid.data.remote.dto.response.account.AccountResponse
+import id.haaweejee.moviedbandroid.data.remote.dto.response.accountstate.AccountStateResponse
 import id.haaweejee.moviedbandroid.data.remote.dto.response.detail.DetailMovieResponse
+import id.haaweejee.moviedbandroid.data.remote.dto.response.genre.GenresResponse
 import id.haaweejee.moviedbandroid.data.remote.dto.response.movielist.MovieResponse
+import id.haaweejee.moviedbandroid.data.remote.dto.response.movielist.MoviesResponse
+import id.haaweejee.moviedbandroid.data.remote.dto.response.rating.PostRatingResponse
 import id.haaweejee.moviedbandroid.data.remote.dto.response.review.ReviewResponse
 import id.haaweejee.moviedbandroid.data.remote.dto.response.review.ReviewsResponse
 import id.haaweejee.moviedbandroid.data.remote.dto.response.video.VideoResponse
@@ -22,4 +28,30 @@ interface MovieDbRepository {
     fun getMovieReviewPagination(movieId: String): Flow<PagingData<ReviewResponse>>
 
     fun getMovieVideo(movieId: String): Flow<VideoResponse>
+
+    suspend fun insertMovieToBookmark(movie: LocalMovieEntity)
+
+    suspend fun deleteMovieBookmarked(movieId: Int)
+
+    fun getMovieBookmarkById(movieId: Int): Flow<LocalMovieEntity?>
+
+    fun getMoviesBookmarked(): Flow<List<LocalMovieEntity>>
+
+    fun getAccountDetail(): Flow<AccountResponse>
+
+    fun getMovieRecommendation(movieId: String): Flow<MoviesResponse>
+
+    fun postMovieRating(movieId: String, rating: Double): Flow<PostRatingResponse>
+
+    fun deleteMovieRating(movieId: String): Flow<PostRatingResponse>
+
+    suspend fun insertRatedMovie(movie: RatedMovieEntity)
+
+    suspend fun deleteRatedMovieById(movieId: Int)
+
+    fun getRatedMovieById(movieId: Int): Flow<RatedMovieEntity?>
+
+    fun getAccountState(movieId: String): Flow<AccountStateResponse>
+
+    fun getMoviesRated(): Flow<PagingData<MovieResponse>>
 }
