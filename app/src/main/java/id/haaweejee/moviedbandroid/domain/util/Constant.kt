f0aa96bd6edaffa360bbfdc472c5cf667008d611
Hendrawan Wibowo Jatmiko <52823787+haaweejee@users.noreply.github.com>
2023-08-21 15:51:55 +0700

    + add new constant

--- app/src/main/java/id/haaweejee/moviedbandroid/domain/util/Constant.kt
+++ app/src/main/java/id/haaweejee/moviedbandroid/domain/util/Constant.kt
@@ -1,3 +1,4 @@
 package id.haaweejee.moviedbandroid.domain.util
 
 val imageUrl = "https://image.tmdb.org/t/p/w500"
+val imagePlaceHolder = "https://www.purnomoyusgiantorocenter.org/wp-content/uploads/2020/06/placeholder.png"
