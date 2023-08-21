4415847e85b239a3132b6ecac2561095eba428e7
Hendrawan Wibowo Jatmiko <52823787+haaweejee@users.noreply.github.com>
2023-08-21 14:59:59 +0700

    + create function for get/post api data

--- app/src/main/java/id/haaweejee/moviedbandroid/data/remote/service/MovieDbNetwork.kt
+++ app/src/main/java/id/haaweejee/moviedbandroid/data/remote/service/MovieDbNetwork.kt
@@ -1,8 +1,11 @@
 package id.haaweejee.moviedbandroid.data.remote.service
 
+import id.haaweejee.moviedbandroid.data.remote.dto.response.account.AccountResponse
+import id.haaweejee.moviedbandroid.data.remote.dto.response.accountstate.AccountStateResponse
 import id.haaweejee.moviedbandroid.data.remote.dto.response.detail.DetailMovieResponse
 import id.haaweejee.moviedbandroid.data.remote.dto.response.genre.GenresResponse
 import id.haaweejee.moviedbandroid.data.remote.dto.response.movielist.MoviesResponse
+import id.haaweejee.moviedbandroid.data.remote.dto.response.rating.PostRatingResponse
 import id.haaweejee.moviedbandroid.data.remote.dto.response.review.ReviewsResponse
 import id.haaweejee.moviedbandroid.data.remote.dto.response.video.VideoResponse
 
@@ -14,4 +17,11 @@ interface MovieDbNetwork {
     suspend fun getMovieReview(movieId: String): ReviewsResponse
     suspend fun getMovieReviewPagination(movieId: String, page: Int): ReviewsResponse
     suspend fun getMovieVideo(movieId: String): VideoResponse
+    suspend fun getAccountDetail(): AccountResponse
+    suspend fun getMovieRecommendation(movieId: String): MoviesResponse
+    suspend fun postMovieRating(movieId: String, rating: Double): PostRatingResponse
+    suspend fun deleteMovieRating(movieId: String): PostRatingResponse
+    suspend fun getAccountState(movieId: String): AccountStateResponse
+    suspend fun getRatedMovie(page: Int): MoviesResponse
+
 }
--- app/src/main/java/id/haaweejee/moviedbandroid/data/remote/service/MovieDbNetworkImpl.kt
+++ app/src/main/java/id/haaweejee/moviedbandroid/data/remote/service/MovieDbNetworkImpl.kt
@@ -1,18 +1,28 @@
 package id.haaweejee.moviedbandroid.data.remote.service
 
 import id.haaweejee.moviedbandroid.BuildConfig
+import id.haaweejee.moviedbandroid.data.remote.dto.request.rating.PostRatingRequest
+import id.haaweejee.moviedbandroid.data.remote.dto.response.account.AccountResponse
+import id.haaweejee.moviedbandroid.data.remote.dto.response.accountstate.AccountStateResponse
 import id.haaweejee.moviedbandroid.data.remote.dto.response.detail.DetailMovieResponse
 import id.haaweejee.moviedbandroid.data.remote.dto.response.genre.GenresResponse
 import id.haaweejee.moviedbandroid.data.remote.dto.response.movielist.MoviesResponse
+import id.haaweejee.moviedbandroid.data.remote.dto.response.rating.PostRatingResponse
 import id.haaweejee.moviedbandroid.data.remote.dto.response.review.ReviewsResponse
 import id.haaweejee.moviedbandroid.data.remote.dto.response.video.VideoResponse
 import io.ktor.client.HttpClient
 import io.ktor.client.call.body
+import io.ktor.client.request.delete
 import io.ktor.client.request.get
 import io.ktor.client.request.header
+import io.ktor.client.request.headers
 import io.ktor.client.request.parameter
+import io.ktor.client.request.post
+import io.ktor.client.request.setBody
 import io.ktor.client.request.url
+import javax.inject.Singleton
 
+@Singleton
 class MovieDbNetworkImpl(
     private val service: HttpClient,
 ) : MovieDbNetwork {
@@ -55,4 +65,46 @@ class MovieDbNetworkImpl(
             url(baseUrl + movieDetail + movieId + movieVideos)
             header("Authorization", "Bearer $accessToken")
         }.body()
+
+    override suspend fun getAccountDetail(): AccountResponse =
+        service.get {
+            url(baseUrl + accountDetail)
+            header("Authorization", "Bearer $accessToken")
+        }.body()
+
+    override suspend fun getMovieRecommendation(movieId: String): MoviesResponse =
+        service.get {
+            url(baseUrl + movieDetail + movieId + moviesRecommendation)
+            header("Authorization", "Bearer $accessToken")
+        }.body()
+
+    override suspend fun postMovieRating(movieId: String, rating: Double): PostRatingResponse =
+        service.post {
+            url(baseUrl + movieDetail + movieId + movieRating)
+            setBody(PostRatingRequest(rating))
+            headers {
+                header("Accept", "application/json")
+                header("Content-Type", "application/json;charset=utf-8")
+                header("Authorization", "Bearer $accessToken")
+            }
+        }.body()
+
+    override suspend fun deleteMovieRating(movieId: String): PostRatingResponse =
+        service.delete {
+            url(baseUrl + movieDetail + movieId + movieRating)
+            header("Authorization", "Bearer $accessToken")
+        }.body()
+
+    override suspend fun getAccountState(movieId: String): AccountStateResponse =
+        service.get {
+            url(baseUrl + movieDetail + movieId + accountState)
+            header("Authorization", "Bearer $accessToken")
+        }.body()
+
+    override suspend fun getRatedMovie(page: Int): MoviesResponse =
+        service.get {
+            url("$baseUrl$accountDetail/8808767$accountRatedMovies")
+            header("Authorization", "Bearer $accessToken")
+        }.body()
 }
+
