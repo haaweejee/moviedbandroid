cd0684cd9dfcc09a6d40aa8d5a8540a37a201cdd
Hendrawan Wibowo Jatmiko <52823787+haaweejee@users.noreply.github.com>
2023-08-21 15:49:33 +0700

    + add new navigation

--- app/src/main/java/id/haaweejee/moviedbandroid/ui/MovieApp.kt
+++ app/src/main/java/id/haaweejee/moviedbandroid/ui/MovieApp.kt
@@ -6,18 +6,27 @@ import androidx.compose.material3.SnackbarHost
 import androidx.compose.material3.SnackbarHostState
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.getValue
+import androidx.compose.runtime.mutableStateOf
 import androidx.compose.runtime.remember
+import androidx.compose.runtime.saveable.rememberSaveable
+import androidx.compose.runtime.setValue
 import androidx.compose.ui.Modifier
 import androidx.navigation.NavHostController
 import androidx.navigation.NavType
 import androidx.navigation.compose.NavHost
 import androidx.navigation.compose.composable
+import androidx.navigation.compose.currentBackStackEntryAsState
 import androidx.navigation.compose.rememberNavController
 import androidx.navigation.navArgument
+import id.haaweejee.moviedbandroid.ui.component.molecules.BottomBar
 import id.haaweejee.moviedbandroid.ui.navigation.Navigation
+import id.haaweejee.moviedbandroid.ui.screen.AboutAppsScreen
+import id.haaweejee.moviedbandroid.ui.screen.AccountScreen
+import id.haaweejee.moviedbandroid.ui.screen.BookmarkScreen
 import id.haaweejee.moviedbandroid.ui.screen.DetailMovieScreen
 import id.haaweejee.moviedbandroid.ui.screen.GenreMoviesScreen
 import id.haaweejee.moviedbandroid.ui.screen.HomeScreen
+import id.haaweejee.moviedbandroid.ui.screen.RatedScreen
 import id.haaweejee.moviedbandroid.ui.theme.MidNight
 import id.haaweejee.moviedbandroid.ui.util.connectivityState
 import kotlinx.coroutines.ExperimentalCoroutinesApi
@@ -31,11 +40,23 @@ fun MovieApp(
     val networkConnectivity by connectivityState()
 
     val snackbarHostState = remember { SnackbarHostState() }
+    var showBottomBar by rememberSaveable { mutableStateOf(true) }
+    val navBackStackEntry by navController.currentBackStackEntryAsState()
+
+    showBottomBar = when (navBackStackEntry?.destination?.route) {
+        Navigation.HomeMovie.route,
+        Navigation.Account.route,
+        -> true
+        else -> false
+    }
 
     Scaffold(
         snackbarHost = { SnackbarHost(hostState = snackbarHostState) },
         modifier = modifier,
         containerColor = MidNight,
+        bottomBar = {
+            if (showBottomBar) BottomBar(navController)
+        },
     ) { innerPadding ->
         NavHost(
             navController = navController,
@@ -58,6 +79,46 @@ fun MovieApp(
                     snackbarHostState = snackbarHostState,
                 )
             }
+            composable(route = Navigation.Account.route) {
+                AccountScreen(
+                    snackbarHostState = snackbarHostState,
+                    navigate = {
+                        when (it) {
+                            "bookmark" -> navController.navigate(Navigation.Bookmark.route)
+                            "rated_movies_tv" -> navController.navigate(Navigation.Rated.route)
+                            "about" -> navController.navigate(Navigation.About.route)
+                        }
+                    },
+                )
+            }
+            composable(route = Navigation.Bookmark.route) {
+                BookmarkScreen(
+                    snackbarHostState = snackbarHostState,
+                    navController = navController,
+                    navigateToDetail = { movieId ->
+                        navController.navigate(
+                            Navigation.DetailMovie.createRoute(movieId),
+                        )
+                    },
+                )
+            }
+            composable(route = Navigation.Rated.route) {
+                RatedScreen(
+                    snackbarHostState = snackbarHostState,
+                    navController = navController,
+                    navigateToDetail = { movieId ->
+                        navController.navigate(
+                            Navigation.DetailMovie.createRoute(movieId),
+                        )
+                    },
+                )
+            }
+            composable(route = Navigation.About.route) {
+                AboutAppsScreen(
+                    snackbarHostState = snackbarHostState,
+                    navController = navController,
+                )
+            }
             composable(
                 route = Navigation.DetailMovie.route,
                 arguments = listOf(navArgument("movieId") { type = NavType.IntType }),
@@ -68,6 +129,11 @@ fun MovieApp(
                     navController = navController,
                     networkConnectivity = networkConnectivity,
                     snackbarHostState = snackbarHostState,
+                    navigateToDetail = { movieId ->
+                        navController.navigate(
+                            Navigation.DetailMovie.createRoute(movieId),
+                        )
+                    },
                 )
             }
             composable(
