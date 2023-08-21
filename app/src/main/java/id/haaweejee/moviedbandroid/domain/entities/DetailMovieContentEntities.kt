322c0daccf5baac88709c8fca6803db065fcf3cb
Hendrawan Wibowo Jatmiko <52823787+haaweejee@users.noreply.github.com>
2023-08-21 15:02:32 +0700

    ! update property data entities

--- app/src/main/java/id/haaweejee/moviedbandroid/domain/entities/DetailMovieContentEntities.kt
+++ app/src/main/java/id/haaweejee/moviedbandroid/domain/entities/DetailMovieContentEntities.kt
@@ -4,4 +4,7 @@ data class DetailMovieContentEntities(
     val header: DetailMovieHeaderEntities,
     val trailer: DetailMovieVideoEntities,
     val review: List<ReviewEntities>,
+    val bookmark: Boolean,
+    val recommendation: List<RecommendationMovieEntities>,
+    val accountState: AccountStateEntities,
 )
--- app/src/main/java/id/haaweejee/moviedbandroid/domain/entities/DetailMovieHeaderEntities.kt
+++ app/src/main/java/id/haaweejee/moviedbandroid/domain/entities/DetailMovieHeaderEntities.kt
@@ -1,8 +1,10 @@
 package id.haaweejee.moviedbandroid.domain.entities
 
 data class DetailMovieHeaderEntities(
+    val idMovie: Int,
     val titleMovie: String,
     val posterMovie: String,
+    val backdropMovie: String,
     val releaseDateMovie: String,
     val ratingMovie: Double,
     val overviewMovie: String,
