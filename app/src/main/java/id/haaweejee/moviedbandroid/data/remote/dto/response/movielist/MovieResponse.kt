8f0d687ee451692d80346cfdeca09d38e1a195af
Hendrawan Wibowo Jatmiko <52823787+haaweejee@users.noreply.github.com>
2023-08-21 14:57:41 +0700

    ! update movie response property

--- app/src/main/java/id/haaweejee/moviedbandroid/data/remote/dto/response/movielist/MovieResponse.kt
+++ app/src/main/java/id/haaweejee/moviedbandroid/data/remote/dto/response/movielist/MovieResponse.kt
@@ -12,10 +12,11 @@ data class MovieResponse(
     val original_title: String,
     val overview: String,
     val popularity: Double,
-    val poster_path: String,
+    val poster_path: String?,
     val release_date: String,
     val title: String,
     val video: Boolean,
     val vote_average: Double,
     val vote_count: Int,
+
 )
